964334816a29567738ebc62ff58386808f0499fa
root <root@sr-dflxapp074.mj.gov.br>
2016-11-01 10:07:33 -0200

    change readme

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 * You can use Web-Server API of CA-Spetrum for add hosts in Maintenace, for it you need GET idspectrum.
 
+
 - For use, you need change schedule.py:
 
+
 ip_spectrum = "10.0.0.1"
+
 user_spectrum = "user"
+
 pass_spectrum = "password"
 
 
--- schedule.py
+++ schedule.py
@@ -6,13 +6,11 @@ from maintenance import Spectrum
 ip_spectrum = "10.0.0.1"
 user_spectrum = "user"
 pass_spectrum = "password"
-
+hostid='x111111'
 
 s=Spectrum(ip_spectrum,user_spectrum,pass_spectrum)
 
 ##Example add maintenance in server of Spectrum.
-print s._add_maintence(hostid[0])
+print s._add_maintence(hostid)
 
-##Example remove maintenance in server of Spectrum.
-#print s._del_maintence(hostid[0])
 
